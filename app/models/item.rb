b722c9044237749df437cd58b5a9b7ce8590e782
enyeldoc2011@gmail.com <enyeldoc2011@gmail.com>
2017-07-19 21:21:46 -0400

    five add scope

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user, optional: true
+
+  scope :size_x, -> (x) { where("sizeItem=?", x)}
+  scope :only_user, -> { where(user_id: nil)}
+  
 end
